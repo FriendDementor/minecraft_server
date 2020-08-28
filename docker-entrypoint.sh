33f6fbf97f840722f3160a77afad16cb07e53912
absentabyss <agna.lumi@protonmail.com>
2020-08-28 18:24:47 -0300

    will send the  command to the server.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,36 @@
-echo Docker is love, Docker is life.
-cd /minecraft
-echo eula=true > eula.txt
-java -Xmx1024M -Xms1024M -jar minecraft_server.jar nogui
+#!/bin/sh
+
+# This function checks whether GNU screen exited gracefully.
+server_wait()
+{
+	while [ true ]
+	do
+		if [ -f "serv_stop" ]; then
+			rm serv_stop
+			break
+		fi
+		sleep 1
+	done
+}
+
+cleanup()
+{
+	echo "Shutting down..."
+	screen -p 0 -X stuff "stop$(echo -ne '\r')"
+	server_wait
+	echo "Graceful shutdown."
+	kill -s SIGINT 1
+}
+trap "cleanup" SIGTERM 	# Triggers "cleanup" on "docker stop."
+
+cd minecraft
+
+if [ ! -f minecraft_server.jar ]; then
+	cp ../minecraft_server.jar .
+fi
+
+echo eula=true > eula.txt 	# Signing EULA is required to launch the Minecraft server.
+screen -d -m sh -c "echo 'Docker is love, Docker is life.' && java -Xmx1024M -Xms1024M -jar minecraft_server.jar nogui && echo 1 > serv_stop" 	# The echo is used by server_wait.
+sleep infinity &
+
+wait $!
